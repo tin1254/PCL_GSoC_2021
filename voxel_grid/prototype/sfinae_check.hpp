f56a9f8502b00a732d5434b47c6b2b1df80e2c34
tin1254 <tin-1254@hotmail.com>
2021-07-25 22:39:11 +0200

    Implement SFINAE check for TransformFilter and GridStruct

---
+++ voxel_grid/prototype/sfinae_check.hpp
@@ -0,0 +1,112 @@
+// #include <iostream>
+#include <optional>
+// #include <string>
+#include <type_traits>
+#include <vector>
+
+template <typename, typename>
+class TransformFilter;
+
+namespace detail {
+template <typename, typename>
+struct IsGridStruct;
+
+template <typename C, typename PointT>
+struct IsGridStruct {
+ private:
+  template <typename T>
+  static constexpr auto hasSetUp(T*) ->
+      typename std::is_same<decltype(std::declval<T>().setUp(
+                                std::declval<TransformFilter<T, PointT>*>())),
+                            bool>::type;
+  template <typename>
+  static constexpr std::false_type hasSetUp(...);
+
+  template <typename T>
+  static constexpr auto hasAddPoint(T*) -> typename std::is_same<
+      decltype(std::declval<T>().addPointToGrid(std::declval<PointT&>())),
+      void>::type;
+  template <typename>
+  static constexpr std::false_type hasAddPoint(...);
+
+  template <typename T>
+  static constexpr auto hasFilter(T*) -> typename std::is_same<
+      decltype(std::declval<T>().filterGrid(
+          std::declval<decltype(std::declval<T>().begin())>())),
+      std::optional<PointT>>::type;
+  template <typename>
+  static constexpr std::false_type hasFilter(...);
+
+  using has_set_up = decltype(hasSetUp<C>(0));
+  using has_add_point = decltype(hasAddPoint<C>(0));
+  using has_filter = decltype(hasFilter<C>(0));
+
+ public:
+  static constexpr bool is_set_up_valid = has_set_up::value;
+  static constexpr bool is_add_point_valid = has_add_point::value;
+  static constexpr bool is_filter_valid = has_filter::value;
+
+  // static constexpr bool is_valid = has_add_point::value && has_set_up::value
+  // && has_filter::value; static constexpr std::string print(){
+  //     return "testing " + is_set_up_valid;
+  // }
+  // static constexpr std::string status = print();
+};
+}  // namespace detail
+
+struct PointXYZ {};
+
+template <typename GridStruct, typename PointT>
+class TransformFilter {
+  using is_grid_struct = detail::IsGridStruct<GridStruct, PointXYZ>;
+  static_assert(is_grid_struct::is_set_up_valid,
+                "Required member is not satisfied: bool "
+                "setUp(TransformFilter<MyGridStruct,PointT>*)");
+  static_assert(
+      is_grid_struct::is_add_point_valid,
+      "Required member is not satisfied: void addPointToGrid(PointT& pt)");
+  static_assert(is_grid_struct::is_filter_valid,
+                "Required member is not satisfied: std::optional<PointXYZ> "
+                "filterGrid(...)");
+
+  void applyFilter() {
+    const bool res = grid_struct_.setUp(this);
+
+    grid_struct_.addPointToGrid(PointT());
+
+    const size_t s = grid_struct_.size();
+
+    for (auto it = grid_struct_.begin(); it != grid_struct_.end(); ++it) {
+      const std::optional<PointT> opt =
+          grid_struct_.filterGrid(grid_struct_.begin());
+    }
+  }
+
+  GridStruct grid_struct_;
+};
+
+template <typename PointT>
+struct MyGridStruct {
+  using Grid = std::vector<size_t>;
+
+  bool setUp(TransformFilter<MyGridStruct, PointT>* f) { return true; }
+
+  size_t size() { return grid_.size(); }
+
+  Grid::iterator begin() { return grid_.begin(); }
+
+  Grid::iterator end() { return grid_.end(); }
+
+  void addPointToGrid(const PointT& pt) {}
+
+  std::optional<PointXYZ> filterGrid(const Grid::iterator grid_it) {
+    return std::nullopt;
+  }
+
+  Grid grid_;
+};
+
+int main() {
+  TransformFilter<MyGridStruct<PointXYZ>, PointXYZ> tf;
+  return 0;
+}
