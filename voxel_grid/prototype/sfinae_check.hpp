76cc5cb8e9949748bff6a55078a1dc53047334f6
tin1254 <tin-1254@hotmail.com>
2021-08-01 02:01:40 +0200

    Add ADL check

--- voxel_grid/prototype/sfinae_check.hpp
+++ voxel_grid/prototype/sfinae_check.hpp
@@ -1,15 +1,15 @@
 // #include <iostream>
-#include <optional>
-// #include <string>
+#include <boost/optional.hpp>
 #include <type_traits>
 #include <vector>
 
 template <typename, typename>
 class TransformFilter;
 
+struct PointXYZ {};
+
 namespace detail {
-template <typename, typename>
-struct IsGridStruct;
+using std::begin;
 
 template <typename C, typename PointT>
 struct IsGridStruct {
@@ -31,17 +31,23 @@ struct IsGridStruct {
 
   template <typename T>
   static constexpr auto hasFilterBegin(T*) -> typename std::is_same<
-      decltype(std::declval<T>().filterGrid(
-          std::declval<decltype(std::declval<T>().begin())>())),
-      std::optional<PointT>>::type;
+      decltype(std::declval<T>().filterGrid(begin(std::declval<T>()))),
+      boost::optional<PointT>>::type;
+  template <typename T>
+  static constexpr auto hasFilterBegin(T*) -> typename std::is_same<
+      decltype(std::declval<T>().filterGrid(std::declval<T>().begin())),
+      boost::optional<PointT>>::type;
   template <typename>
   static constexpr std::false_type hasFilterBegin(...);
 
   template <typename T>
   static constexpr auto hasFilterEnd(T*) -> typename std::is_same<
-      decltype(std::declval<T>().filterGrid(
-          std::declval<decltype(std::declval<T>().end())>())),
-      std::optional<PointT>>::type;
+      decltype(std::declval<T>().filterGrid(end(std::declval<T>()))),
+      boost::optional<PointT>>::type;
+  template <typename T>
+  static constexpr auto hasFilterEnd(T*) -> typename std::is_same<
+      decltype(std::declval<T>().filterGrid(std::declval<T>().end())),
+      boost::optional<PointT>>::type;
   template <typename>
   static constexpr std::false_type hasFilterEnd(...);
 
@@ -65,47 +71,15 @@ struct IsGridStruct {
       has_filter_begin::value && has_filter_end::value;
   static constexpr bool is_size_valid = has_size::value;
 
-  // static constexpr bool is_valid = has_add_point::value && has_set_up::value
-  // && has_filter::value; static constexpr std::string print(){
-  //     return "testing " + is_set_up_valid;
-  // }
-  // static constexpr std::string status = print();
+  static constexpr bool is_valid =
+      is_set_up_valid && is_add_point_valid && is_filter_valid && is_size_valid;
 };
 }  // namespace detail
 
-struct PointXYZ {};
-
-template <typename GridStruct, typename PointT>
-class TransformFilter {
-  using is_grid_struct = detail::IsGridStruct<GridStruct, PointXYZ>;
-  static_assert(is_grid_struct::is_set_up_valid,
-                "Required member is not satisfied: bool "
-                "setUp(TransformFilter<MyGridStruct,PointT>*)");
-  static_assert(
-      is_grid_struct::is_add_point_valid,
-      "Required member is not satisfied: void addPointToGrid(PointT& pt)");
-  static_assert(is_grid_struct::is_filter_valid,
-                "Required member is not satisfied: std::optional<PointXYZ> "
-                "filterGrid(begin()) or std::optional<PointXYZ> "
-                "filterGrid(end())");
-  static_assert(is_grid_struct::is_size_valid,
-                "Required member is not satisfied: std::size_t size()");
-
-  void applyFilter() {
-    const bool res = grid_struct_.setUp(this);
-
-    grid_struct_.addPointToGrid(PointT());
-
-    const size_t s = grid_struct_.size();
-
-    for (auto it = grid_struct_.begin(); it != grid_struct_.end(); ++it) {
-      const std::optional<PointT> opt =
-          grid_struct_.filterGrid(grid_struct_.begin());
-    }
-  }
-
-  GridStruct grid_struct_;
-};
+template <typename GridStruct>
+auto begin(GridStruct grid_struct) -> decltype(grid_struct.grid_.begin()) {
+  return grid_struct.grid_.begin();
+}
 
 template <typename PointT>
 struct MyGridStruct {
@@ -115,20 +89,48 @@ struct MyGridStruct {
 
   size_t size() { return grid_.size(); }
 
-  Grid::iterator begin() { return grid_.begin(); }
+  //   Grid::iterator begin() { return grid_.begin(); }
 
   Grid::iterator end() { return grid_.end(); }
 
   void addPointToGrid(const PointT& pt) {}
 
-  std::optional<PointXYZ> filterGrid(const Grid::iterator grid_it) {
-    return std::nullopt;
+  boost::optional<PointT> filterGrid(const Grid::iterator grid_it) {
+    return boost::optional<PointT>();
   }
 
   Grid grid_;
 };
 
+template <typename GridStruct, typename PointT>
+class TransformFilter {
+  using is_grid_struct = detail::IsGridStruct<GridStruct, PointT>;
+  static_assert(is_grid_struct::is_valid,
+                "GridStruct requirement is not satisfied");
+
+ public:
+  void applyFilter() {
+    const bool res = grid_struct_.setUp(this);
+
+    grid_struct_.addPointToGrid(PointT());
+
+    const size_t s = grid_struct_.size();
+
+    using std::begin;  // invoke ADL, just like ranged-for loop
+    using std::end;
+    auto it = begin(grid_struct_);
+    auto end_it = end(grid_struct_);
+
+    for (; it != end_it; ++it) {
+      const boost::optional<PointT> opt = grid_struct_.filterGrid(it);
+    }
+  }
+
+  GridStruct grid_struct_;
+};
+
 int main() {
   TransformFilter<MyGridStruct<PointXYZ>, PointXYZ> tf;
+  tf.applyFilter();
   return 0;
 }
